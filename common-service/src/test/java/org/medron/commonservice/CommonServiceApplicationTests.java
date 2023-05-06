83f18119b82e4be92d584b3ad3710b4eb7d9792a
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-05-06 13:23:49 +0300

    Initial commit

---
+++ common-service/src/test/java/org/medron/commonservice/CommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medron.commonservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ stock-service/src/main/java/org/medron/stockservice/StockServiceApplication.java
@@ -0,0 +1,13 @@
+package org.medron.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockServiceApplication.class, args);
+	}
+
+}
