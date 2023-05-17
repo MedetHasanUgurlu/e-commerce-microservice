9d1c009ee124fca97cd0fb9ff9ce845ccfce5c37
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-05-17 23:53:28 +0300

    Product entity

---
+++ stock-service/src/main/java/org/medron/stockservice/entity/Category.java
@@ -0,0 +1,23 @@
+package org.medron.stockservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+}
